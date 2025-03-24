f818c6ff6b4efb5f4628d63e804e75bc3f2c16a3
JustPanka <annarendes@gmail.com>
2025-03-24 21:47:55 +0800

    Update myScript.js
    Start to write function playround.

--- myScript.js
+++ myScript.js
@@ -19,3 +19,44 @@ function getHumanChoice() {
 
 let humanScore = 0,
     computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    console.log(humanChoice);
+    if (humanChoice === computerChoice) {
+        prompt('It is a tie, try again!');
+
+    } else {
+        switch(humanChoice) {
+            case "rock":
+                if (computerChoice === "paper") {
+                    console.log("You lost, paper beats rock.");
+                    computerScore++;
+                    
+                } else {
+                    console.log("You won, rock beats scissors.")
+                    humanScore++;
+                }
+                break;
+            case "paper":
+                if (computerChoice === "scissors") {
+                    console.log("You lost, scissors beats paper.");
+                    computerScore++;
+                    
+                } else {
+                    console.log("You won, paper beats rock.")
+                    humanScore++;
+                }
+              break;
+            default:
+              // code block
+        }
+    }
+
+    return prompt('You lose! Paper beats Rock.')
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
